d3f116898b9963bf320dafbca22884105ee051bf
Dr Ali <khalili1384@gmail.com>
2026-02-24 03:06:56 -0800

    chore: remove experimental scripts and runtime artifacts

--- test_db.py
+++
@@ -1,9 +0,0 @@
-﻿import sqlite3
-
-con = sqlite3.connect(r"src\clincore\mcare_engine\data\synthesis.135.db")
-cur = con.cursor()
-
-cur.execute("SELECT name FROM sqlite_master WHERE type='table'")
-print("TABLES:", cur.fetchall())
-
-con.close()
